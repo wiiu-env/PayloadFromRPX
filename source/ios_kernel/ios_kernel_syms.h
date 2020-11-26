ee792d389c564d61495cbe43747c1a64a922ff9e
Maschell <Maschell@gmx.de>
2020-11-26 22:03:02 +0100

    Update the .gitignore

--- source/ios_kernel/ios_kernel_syms.h
+++
@@ -1,7 +0,0 @@
-#ifndef ios_kernel_SYMS_H
-#define ios_kernel_SYMS_H
-#define instant_patches_setup 0x08135004
-#define reverse_memcpy 0x08135178
-#define _main 0x08135044
-#define __KERNEL_CODE_START 0x08135000
-#endif
